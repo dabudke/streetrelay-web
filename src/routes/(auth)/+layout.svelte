fb1863dade8ac3c53ce8c00b262c34384bc0c662
LittleKitacho <40306561+LittleKitacho@users.noreply.github.com>
2023-11-05 21:16:24 -0500

    update auth route layout

--- src/routes/(auth)/+layout.svelte
+++ src/routes/(auth)/+layout.svelte
@@ -1,14 +1,29 @@
-<div class="card">
+<form
+  class="card"
+  method="post"
+  novalidate
+>
   <slot />
-</div>
+</form>
 
 <style>
+  :global(body) {
+    min-height: 100vh;
+    width: 100vw;
+    background: linear-gradient(
+      to bottom right,
+      color-mix(in srgb, var(--primary), var(--background) 80%),
+      var(--background)
+    );
+    margin: 0;
+    padding: 3rem;
+  }
   .card {
     border-radius: 2rem;
     padding: 2rem;
-    background-color: var(--secondary);
-    margin: 3rem auto;
-    width: fit-content;
+    background-color: var(--background);
+    margin: 0 auto;
+    max-width: 30rem;
     box-shadow: 0 3px 10px -2px rgba(0, 0, 0, 0.4);
   }
 </style>
