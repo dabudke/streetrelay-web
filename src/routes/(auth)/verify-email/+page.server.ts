c3ff84b22e57d81ec00f7508e9ff92bf3d777d3a
Daniel Budke <dbudke1010@outlook.com>
2024-01-11 23:36:24 -0500

    add email verification page

---
+++ src/routes/(auth)/verify-email/+page.server.ts
@@ -0,0 +1,80 @@
+import type { PageServerLoad } from "./$types";
+import prisma from "$lib/server/prisma";
+
+export const load: PageServerLoad = async ({ url }) => {
+  const token = url.searchParams.get("t");
+  if (!token) {
+    return {
+      error: true,
+      message: "Click the link in your email to verify your email address.",
+    };
+  }
+
+  const verification = await prisma.emailVerification.findUnique({
+    where: {
+      token,
+    },
+    include: {
+      user: true,
+    },
+  });
+  if (!verification) {
+    return {
+      error: true,
+      message:
+        "An error occured while trying to verify your email. Please try again later.",
+    };
+  }
+  if (verification.expires <= new Date()) {
+    prisma.emailVerification
+      .delete({
+        where: {
+          token,
+        },
+      })
+      .catch(console.error);
+    return {
+      error: true,
+      message: "This link has expired. Please resend the verification email.",
+    };
+  }
+
+  // Email verified!
+
+  try {
+    prisma.user.update({
+      where: {
+        id: verification.userId,
+      },
+      data: {
+        email: verification.email,
+      },
+    });
+  } catch (e) {
+    console.error(e);
+    return {
+      error: true,
+      message:
+        "An error occured while updating your email address. Please try again later.",
+    };
+  }
+
+  try {
+    await prisma.emailVerification.delete({
+      where: {
+        token,
+      },
+    });
+  } catch (e) {
+    console.error(e);
+    return {
+      error: true,
+      message: "Your email has not been updated, please try again later.",
+    };
+  }
+
+  return {
+    error: false,
+    message: `Email successfully updated to ${verification.email}. You may now close this tab.`,
+  };
+};
---
+++ src/routes/(auth)/verify-email/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import type { PageData } from "./$types";
+
+  export let data: PageData;
+</script>
+
+<p class:error={data.error}>{data.message}</p>
+
+<style>
+  p {
+    display: block;
+    background-color: color-mix(in srgb, var(--background), var(--primary) 25%);
+    color: color-mix(in srgb, var(--text), var(--primary) 10%);
+    padding: 1.5rem 2rem;
+    border-radius: 0.7rem;
+  }
+  .error {
+    background-color: color-mix(in srgb, var(--background), var(--error) 30%);
+    color: color-mix(in srgb, var(--text), var(--error) 10%);
+  }
+</style>
