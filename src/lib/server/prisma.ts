2fe779d17a34ce07af4099c3b8a36d5d2f1a61d0
Daniel Budke <dbudke1010@outlook.com>
2024-04-20 15:09:47 -0400

    remove prisma connection log statement

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -4,5 +4,4 @@ export default prisma;
 
 prisma
   .$connect()
-  .then(() => console.log("Connected to MongoDB"))
-  .catch(() => console.error("Could not connect to MongoDB!"));
+  .catch((err) => console.error("Could not connect to MongoDB!", err));
